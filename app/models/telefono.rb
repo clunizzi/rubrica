968ca39c92e925eae06886c3f3d2e17bba2c4246
Andrea Tozzini <a.tozzini@gmail.com>
2015-12-03 22:57:44 +0100

    Modify carrierwave config

--- app/models/telefono.rb
+++ app/models/telefono.rb
@@ -2,6 +2,6 @@ class Telefono < ActiveRecord::Base
 	belongs_to :contatti
 	VALIDAZIONE_TELEFONO_REGEXP = /\A\+?[0-9][^a-z]+\z/i
 	validates :numero_di_telefono, presence: true,
-	                               length: { minimum: 10, maximum: 17},
+	                               length: { minimum: 9, maximum: 17},
 	                               format: { with: VALIDAZIONE_TELEFONO_REGEXP }
 end
